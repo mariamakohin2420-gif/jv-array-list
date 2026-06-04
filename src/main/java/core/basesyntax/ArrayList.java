b9869a8f810d7dd22928c8c2b06d8346ad1b7112
Mariia Makohin <maria.makohin2420@gmail.com>
2026-06-04 09:08:42 +0300

    solution

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,132 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private Object[] elementData;
+    private int size;
+
+    public ArrayList() {
+        this.elementData = new Object[DEFAULT_CAPACITY];
+        this.size = 0;
+
+    }
     @Override
     public void add(T value) {
+        ensureCapacity();
+        elementData[size++] = value;
 
     }
 
     @Override
     public void add(T value, int index) {
+        checkIndexForAdd(index);
+        ensureCapacity();
+
+        System.arraycopy(elementData, index, elementData, index + 1, size - index);
+        elementData[index] = value;
+        size++;
 
     }
 
     @Override
     public void addAll(List<T> list) {
+        if (list == null) {
+            return;
+        }
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
 
     }
 
     @Override
     public T get(int index) {
-        return null;
+
+        checkIndex(index);
+        return (T) elementData[index];
     }
 
     @Override
     public void set(T value, int index) {
+        checkIndex(index);
+        elementData[index] = value;
 
     }
 
     @Override
     public T remove(int index) {
-        return null;
+
+        checkIndex(index);
+        T oldVal = (T) elementData[index];
+
+        int numMoved = size - index - 1;
+        if (numMoved > 0) {
+            System.arraycopy(elementData, index + 1, elementData, index, numMoved);
+
+        }
+        elementData[--size] = null;
+        return oldVal;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+
+        int index = findIndex(element);
+        if (index == -1) {
+            throw new NoSuchElementException("Element not found in ArrayList: " + element);
+        }
+        return remove(index);
     }
 
     @Override
     public int size() {
-        return 0;
+
+        return this.size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+
+        return this.size == 0;
+    }
+    private int findIndex(T element) {
+        if (element == null) {
+            for (int i = 0; i < size; i++) {
+                if (elementData[i] == null) {
+                    return i;
+                }
+            }
+            } else {
+                for (int i = 0; i < size; i++) {
+                    if (element.equals(elementData[i])) {
+                        return i;
+                }
+            }
+        }
+        return -1;
+    }
+    private void ensureCapacity() {
+        if (size == elementData.length) {
+            int oldCapacity = elementData.length;
+            int newCapacity = oldCapacity + (oldCapacity >> 1);
+            if (newCapacity == 0) {
+                newCapacity = 1;
+            }
+            Object[] newArray = new Object[newCapacity];
+            System.arraycopy(elementData, 0, newArray, 0, size);
+            elementData = newArray;
+        }
+    }
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("Invalid index: " + index + ", Size: " + size);
+        }
+    }
+    private void checkIndexForAdd(int index) {
+        if (index < 0 || index > size) {
+            throw new ArrayListIndexOutOfBoundsException("Invalid index for add: " + index + ", Size: " + size);
+        }
     }
 }
